01e8b2c6f4d6197dc97c003faa55ec5619a66779
SHjalilo <aaaabdjalil@gmail.com>
2023-03-30 16:20:46 +0000

    1st commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Creating Fake Information
+
+If you ever need to create a fake person, this faker library provides you with a Faker class that automatically generates an entire person. This script creates a couple of different people and stores them in a Data Frame, which is a slightly more complicated concept. 
---
+++ script.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from faker import Faker
+
+# Create object
+fake = Faker()
+
+# Generate data
+fake.name()
+fake.text()
+fake.address()
+fake.email()
+fake.date()
+fake.country()
+fake.phone_number()
+fake.random_number(digits=5)
+
+# Dataframe creation
+fakeDataframe = pd.DataFrame({
+                                #'date':[fake.date() for i in range(5)],
+                                'name':[fake.name() for i in range(5)],
+                                'email':[fake.email() for i in range(5)],
+                                'phone number':[fake.phone_number() for i in range(5)],
+                                'address':[fake.address() for i in range(5)]
+                            })
+print(fakeDataframe)
